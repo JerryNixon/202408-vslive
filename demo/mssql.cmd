2cb467fb213d842fd8275a5dbcb8a2397c196824
Jerry Nixon <jerry.nixon@microsoft.com>
2024-08-02 23:07:26 -0600

    Update

--- demo/mssql.cmd
+++ demo/mssql.cmd
@@ -1,3 +1,5 @@
+sqlcmd delete --force
+
 SET SQLCMD_ACCEPT_EULA=YES
 SET "SQLCMD_BAK=https://github.com/JerryNixon/202408-vslive/raw/main/HR.bak"
 sqlcmd.exe create mssql --using %SQLCMD_BAK% --port 1234 --cached
